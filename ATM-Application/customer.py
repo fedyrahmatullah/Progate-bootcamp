75081b446a5b892677e541bc5c020eff236f146f
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2021-10-01 17:32:40 +0700

    update cust pin

--- ATM-Application/customer.py
+++ ATM-Application/customer.py
@@ -1,1 +1,7 @@
 from atm_card import ATMCard
+
+class Customer:
+    def __init__ (self, id, custPin = 1234, custBalance = 10000):
+        self.id = id
+        self.pin = custPin
+        self.balance = custBalance
