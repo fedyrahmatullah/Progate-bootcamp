00bb531489b0b9392ab460927fe8b81ef1ba9412
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2021-10-01 17:36:25 +0700

    Update customer.py

--- ATM-Application/customer.py
+++ ATM-Application/customer.py
@@ -5,3 +5,17 @@ class Customer:
         self.id = id
         self.pin = custPin
         self.balance = custBalance
+        
+        def checkId(self):
+            return self.id
+        def checkPin(self):
+            return self.pin
+        def checkBalance(self):
+            return self.balance
+        def withdrawBalance(self, nominal):
+            self.balance -= nominal
+        def depositBalance(self, nominal):
+            self.balance += nominal
+            
+        def Error(self):
+            return LookupError.with_traceback()
