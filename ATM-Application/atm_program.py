6bbfc52f3c2e93882afcd03a20617ffa68a80d05
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2021-10-01 18:05:50 +0700

    update atm_program.py

--- ATM-Application/atm_program.py
+++ ATM-Application/atm_program.py
@@ -15,3 +15,33 @@ while True:
         if trial == 3:
             print("Error. Silahkan ambil kartu dan coba lagi..")
             exit()
+            
+        while True:
+            print("Selamat datang di ATM Progate..")
+            print("\n1 - Cek Saldo \t 2 - Debet \t 3 - Simpan \t 4 - Ganti Pin \t 5 - Keluar ")
+            
+            selectmenu = int(input("\nSilahkan pilih menu: "))
+            
+            if selectmenu == 1:
+                print("\nSaldo anda sekarang: Rp. " + str(atm.checkbalance()) + "\n")
+            elif selectmenu == 2:
+                nominal = float(input("Masukkan nominal saldo: "))
+                verify_withdraw = input("Konfirmasi: Anda akan melakukan debet dengan nominal berikut ? y/n " + str(nominal) + " ")
+                
+                if verify_withdraw == "y":
+                    print("Saldo awal anda adalah: Rp. " + str(atm.checkBalance()) + "")
+                else:
+                    break
+                if nominal < atm.checkBalance():
+                    atm.withdrawBalance(nominal)
+                    print("Transaksi debet berhasil!")
+                    print("Saldo sisa anda sekarang: Rp. " + str(atm.checkBalance()) + "")
+                else:
+                    print("Maaf. Saldo anda tidak cukup untuk melakukan debet!")
+                    print("Silahkan lakukan penambahan nominal saldo")
+            elif selectmenu == 3:
+            elif selectmenu == 4:
+            elif selectmenu == 5:
+            else:
+            
+            
